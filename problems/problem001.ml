8af55058dce15214c46c5f1c4e2908b2ccca6ca8
Alex Robbins <alexander.j.robbins@gmail.com>
2012-01-30 19:45:47 -0600

    Dropping some unnecessary semicolons.

--- problems/problem001.ml
+++ problems/problem001.ml
@@ -7,12 +7,12 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 let amount = function
   | n when n mod 3 == 0 -> n
   | n when n mod 5 == 0 -> n
-  | _ -> 0;;
+  | _ -> 0
 
 let rec sum current top accum = 
     match current == top with
       true -> accum
-      | false -> sum (current + 1) top (accum + (amount current));;
+      | false -> sum (current + 1) top (accum + (amount current))
 
 let problem1 top =
   sum 1 top 0;;
