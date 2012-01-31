f8eb59d398108de08103f26ca5e0583a195e002d
Alex Robbins <alexander.j.robbins@gmail.com>
2012-01-30 19:30:48 -0600

    Problem 1 is working, but it seems really ugly.

---
+++ problems/problem001.ml
@@ -0,0 +1,19 @@
+(*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*)
+
+let amount = function
+  | n when n mod 3 == 0 -> n
+  | n when n mod 5 == 0 -> n
+  | _ -> 0;;
+
+let rec sum current top accum = 
+  let over = current == top in
+    match over with
+      true -> accum
+      | false -> sum (current + 1) top (accum + (amount current));;
+
+let problem1 top =
+  sum 1 top 0;;
