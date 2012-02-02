9497b789e01167f70e428bd3ca84bce3e518f1bd
Alex Robbins <alexander.j.robbins@gmail.com>
2012-02-01 22:03:00 -0600

    Clean up the first problem. Maybe more idiomatic?

--- problems/problem001.ml
+++ problems/problem001.ml
@@ -4,15 +4,14 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 *)
 
-let amount = function
-  | n when n mod 3 == 0 -> n
-  | n when n mod 5 == 0 -> n
-  | _ -> 0
-
 let rec sum current top accum = 
-    match current == top with
-      true -> accum
-      | false -> sum (current + 1) top (accum + (amount current))
+  let amount = function
+      n when n mod 3 = 0 or n mod 5 = 0 -> n
+    | _ -> 0
+  in
+  match current = top with
+    true -> accum
+    | false -> sum (current + 1) top (accum + (amount current))
 
 let problem1 top =
   sum 1 top 0;;
